14c00eee91a0efc3fea71d607cd8e5ac4382565d
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-10-28 18:54:50 -0300

    Setting up User model with JPA

--- src/main/java/com/carterdboyle/store/StoreApplication.java
+++ src/main/java/com/carterdboyle/store/StoreApplication.java
@@ -1,21 +1,13 @@
 package com.carterdboyle.store;
 
-import com.carterdboyle.store.entities.User;
-import com.carterdboyle.store.repositories.UserRepository;
-import com.carterdboyle.store.services.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     static void main(String[] args) {
-
-        ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var userService = context.getBean(UserService.class);
-        userService.register(new User(1L, "test@example.com", "password123", "John Smith"));
+        SpringApplication.run(StoreApplication.class, args);
     }
 
 }
--- src/main/resources/db/migration/V1__initial_migration.sql
+++ src/main/resources/db/migration/V1__initial_migration.sql
@@ -3,7 +3,7 @@ create table users
     id       bigint auto_increment
         primary key,
     name     varchar(255) not null,
-    email    varchar(255) null,
+    email    varchar(255) not null,
     password varchar(255) not null
 );
 
@@ -11,11 +11,10 @@ create table addresses
 (
     id      bigint auto_increment
         primary key,
-    street  varchar(255) null,
-    city    varchar(255) null,
-    zip     varchar(255) null,
+    street  varchar(255) not null,
+    city    varchar(255) not null,
+    zip     varchar(255) not null,
     user_id bigint       null,
-    constraint addresses_users_id_fk
-        foreign key (user_id) references users (id)
+    foreign key (user_id) references users (id)
 );
 
