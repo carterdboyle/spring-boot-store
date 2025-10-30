7c472779d56b198db6ce13cd2234469a85cc2604
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-10-30 11:44:32 -0300

    Preparing to use JPA to auto-generate tables

--- src/main/java/com/carterdboyle/store/StoreApplication.java
+++ src/main/java/com/carterdboyle/store/StoreApplication.java
@@ -1,7 +1,5 @@
 package com.carterdboyle.store;
 
-import com.carterdboyle.store.entities.Category;
-import com.carterdboyle.store.entities.Product;
 import com.carterdboyle.store.entities.Profile;
 import com.carterdboyle.store.entities.User;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,21 +24,6 @@ public class StoreApplication {
 
         user.setProfile(profile);
         profile.setUser(user);
-
-        var product = new Product();
-        product.setId(1L);
-        product.setName("Screwdriver");
-        product.setPrice(new BigDecimal("10.12"));
-
-        var category = new Category();
-        category.setId((byte) 1);
-        category.setName("Tools and Hardware");
-
-        product.setCategory(category);
-
-        user.addTag("tag1");
-        System.out.println(user);
-        System.out.println(product);
 
     }
 
