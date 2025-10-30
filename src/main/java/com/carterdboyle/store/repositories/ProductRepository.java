17e43cb18bdf8dda3c1ca7a2d0325ae71eeb0950
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-10-30 16:59:01 -0300

    Adding in some derived query methods into repository

--- src/main/java/com/carterdboyle/store/repositories/ProductRepository.java
+++ src/main/java/com/carterdboyle/store/repositories/ProductRepository.java
@@ -3,5 +3,38 @@ package com.carterdboyle.store.repositories;
 import com.carterdboyle.store.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, Long> {
+    // String - want to use like in SQL
+    List<Product> findByName(String name);
+    List<Product> findByNameLike(String name);
+    List<Product> findByNameContaining(String name);
+    List<Product> findByNameStartingWith(String name);
+    List<Product> findByNameEndingWith(String name);
+    List<Product> findByNameEndingWithIgnoreCase(String name);
+
+    //Numbers
+    List<Product> findByPrice(BigDecimal price);
+    List<Product> findByPriceGreaterThan(BigDecimal price);
+    List<Product> findByPriceGreaterThanEqual(BigDecimal price);
+    List<Product> findByPriceLessThan(BigDecimal price);
+    List<Product> findByPriceLessThanEqual(BigDecimal price);
+    List<Product> findByPriceBetween(BigDecimal price1, BigDecimal price2);
+
+    // Null
+    List<Product> findByDescriptionNull();
+    List<Product> findByDescriptionNotNull();
+
+    // Multiple Conditions
+    List<Product> findByDescriptionNullAndNameNull();
+
+    // Sort (order by)
+    List<Product> findByNameOrderByPrice(String name);
+    List<Product> findByNameOrderByPriceAsc(String name);
+
+    // Limit (Top/First)
+    List<Product> findTop5ByNameOrderByPrice(String name);
+    List<Product> findFirst5ByNameLikeOrderByPrice(String name);
 }
