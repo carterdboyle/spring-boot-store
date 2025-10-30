0985666085b94be4ec4a6fc13ccd6962f58940d0
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-10-30 12:01:21 -0300

    Creating a flyway migration using a model-first approach with JPA buddy
    For adding a description this time

---
+++ src/main/java/com/carterdboyle/store/entities/Product.java
@@ -0,0 +1,32 @@
+package com.carterdboyle.store.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT", nullable = false)
+    private String description;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
