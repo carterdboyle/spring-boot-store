9dddce7a512b967f62b1d7c5a107f7706feb89dc
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-10-31 10:43:00 -0300

    Using specifications to build queries

--- src/main/java/com/carterdboyle/store/StoreApplication.java
+++ src/main/java/com/carterdboyle/store/StoreApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.math.BigDecimal;
+
 @SpringBootApplication
 public class StoreApplication {
 
     static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
         var service = context.getBean(ProductService.class);
-        service.fetchProductsByCriteria();
+        service.fetchProductsBySpecification("prod", BigDecimal.valueOf(1), null);
     }
 }
--- src/main/java/com/carterdboyle/store/services/ProductService.java
+++ src/main/java/com/carterdboyle/store/services/ProductService.java
@@ -6,10 +6,12 @@ import com.carterdboyle.store.repositories.CategoryRepository;
 import com.carterdboyle.store.repositories.ProductRepository;
 import com.carterdboyle.store.repositories.ProfileRepository;
 import com.carterdboyle.store.repositories.UserRepository;
+import com.carterdboyle.store.repositories.specifications.ProductSpec;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -87,5 +89,23 @@ public class ProductService {
     public void fetchProductsByCriteria() {
         var products = productRepository.findProductsByCriteria("prod", BigDecimal.valueOf(1), null);
         products.forEach(System.out::println);
+    }
+
+    public void fetchProductsBySpecification(String name, BigDecimal minPrice, BigDecimal maxPrice) {
+        Specification<Product> spec = Specification.allOf();
+
+        if (name != null) {
+            spec = spec.and(ProductSpec.hasName(name));
+        }
+
+        if (minPrice != null) {
+            spec = spec.and(ProductSpec.hasPriceGreaterThanOrEqualTo(minPrice));
+        }
+
+        if (maxPrice != null) {
+            spec = spec.and(ProductSpec.hasPriceLessThanOrEqualTo(maxPrice));
+        }
+
+        productRepository.findAll(spec).forEach(System.out::println);
     }
 }
