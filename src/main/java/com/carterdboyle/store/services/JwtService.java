469b7d6139cf58a81f58e80e2a553ca2206f75e1
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-11-05 12:00:02 -0400

    Setting up dot env for Spring and including JWT secret

--- src/main/java/com/carterdboyle/store/services/JwtService.java
+++ src/main/java/com/carterdboyle/store/services/JwtService.java
@@ -1,20 +1,23 @@
 package com.carterdboyle.store.services;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
     public String generateToken(String email) {
         final long tokenExpiration = 86400; // 1 day in seconds
 
         return Jwts.builder().subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-                .signWith(Keys.hmacShaKeyFor("secret".getBytes())).compact();
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes())).compact();
     }
 }
