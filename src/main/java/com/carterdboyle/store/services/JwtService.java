bdccc93035856ec4196f4ba6388c29d14f6295f0
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-11-07 12:37:03 -0400

    Changed config - JWT expires after 15 mins now for testing, adding role to jwt payload to check endpoints later on

--- src/main/java/com/carterdboyle/store/services/JwtService.java
+++ src/main/java/com/carterdboyle/store/services/JwtService.java
@@ -27,6 +27,7 @@ public class JwtService {
         return Jwts.builder().subject(user.getId().toString())
                 .claim("email", user.getEmail())
                 .claim("name", user.getName())
+                .claim("role", user.getRole().toString())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
                 .signWith(jwtConfig.getSecretKey()).compact();
